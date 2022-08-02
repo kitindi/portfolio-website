eeb196d110f141ecbb5caa6e6ebc03aaaf3d1515
kitindi <abdullkitindi@gmail.com>
2022-08-03 01:24:11 +0300

    updated the mobile version

--- js/main.js
+++ js/main.js
@@ -5,3 +5,16 @@ window.addEventListener("scroll", () => {
 
   header.classList.toggle("gb-white", window.scrollY > 0);
 });
+
+let day = new Date().getFullYear();
+document.querySelector(".showDate").textContent = day;
+
+function toggleMenu(e) {
+  let mobileMenu = document.querySelector(".mobile-menu");
+  mobileMenu.classList.toggle("hidden");
+}
+
+function toggleMobileMenu(e) {
+  let mobileMenu = document.querySelector(".mobile-menu");
+  mobileMenu.classList.toggle("hidden");
+}
